8259a3bbcdad3b00a31fe52eacc2caba1ed8eb14
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-07-01 18:45:26 -0700

    Provide full argument name

--- src/library/tools/R/check.R
+++ src/library/tools/R/check.R
@@ -2933,10 +2933,10 @@ add_dummies <- function(dir, Log)
             any <- msg <- character()
             for (m in makefiles) {
                 lines <- readLines(m, warn = FALSE)
-                have_c <- length(dir('src', patt = "[.]c$", recursive = TRUE)) > 0L
-                have_cxx <- length(dir('src', patt = "[.](cc|cpp)$", recursive = TRUE)) > 0L
-                have_f <- length(dir('src', patt = "[.]f$", recursive = TRUE)) > 0L
-                have_f9x <- length(dir('src', patt = "[.]f9[05]$", recursive = TRUE)) > 0L
+                have_c <- length(dir('src', pattern = "[.]c$", recursive = TRUE)) > 0L
+                have_cxx <- length(dir('src', pattern = "[.](cc|cpp)$", recursive = TRUE)) > 0L
+                have_f <- length(dir('src', pattern = "[.]f$", recursive = TRUE)) > 0L
+                have_f9x <- length(dir('src', pattern = "[.]f9[05]$", recursive = TRUE)) > 0L
                 for (f in c("C", "CXX", "F", "FC", "CPP"))  {
                     this <- paste0(f, "FLAGS")
                     this2 <- paste0("PKG_", this)
